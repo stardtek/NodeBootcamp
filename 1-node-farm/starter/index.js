a114107060faea1b4f865d4379f1ef662919b988
stardtek <z.ostroznik@gmail.com>
2020-09-29 15:35:02 +0200

    Lecture 16 done

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -26,20 +26,66 @@ console.log("File wriete."); */
     });
 });
 console.log("will read file"); */
+
 /////////////////////////////////////
 ///////////// SERVER
+const tempOverviw = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
+
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic');
+        
+    }
+    return output;
+    
 
 
+
+}
 
 const server = http.createServer((req, res)=>{
-    const pathname = req.url;
+    
+    const { query, pathname } = url.parse(req.url, true);
+    
+    //overview
     if(pathname === '/' || pathname === '/overview'){
-        res.end('This is overview');    
+        res.writeHead(200, {'Content-type': 'text/html'})
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el) ).join('');
+        const output = tempOverviw.replace('{%PRODUCT_CARDS%}',cardsHtml);
+        
+
+
+        res.end(output)
+
+
+
+    //product
     }else if( pathname === '/product'){
-        res.end('This is product');
+        //console.log(query);
+        //console.log(url.parse(req.url, true));
+        res.writeHead(200, {'Content-type': 'text/html'})
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct,product);
+        
+
+
+        res.end(output);
     }
+    //api
     else if(pathname === '/api'){
 
         
