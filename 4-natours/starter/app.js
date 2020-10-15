6e107d34e20f91d024c8a2a7629f43a760b41f6a
stardtek <z.ostroznik@gmail.com>
2020-10-15 21:42:14 +0200

    natours

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('Post');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}...`);
+});
